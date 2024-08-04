bbdb4fce29bd867b59250724fa1c1bc4ecf0a28b
Franco <francomercau91@gmail.com>
2024-08-04 21:11:02 +0200

    ajout des funcionalites des recuperation de dones - rating match

--- scraptest.py
+++ scraptest.py
@@ -50,26 +50,36 @@ table_info = soup.select_one('table.table')
 
 infos_product = table_info.find_all('tr')
 
-rating = soup.select_one('p', class_ = 'star-rating One') 
 
-match rating :
-    case ('star-rating One'):
-        print('Rating is 1/5')
-    case ('star-rating Two'):
-        print('Rating is 1/5')
-    case ('star-rating Three'):
-        print('Rating is 1/5')
-    case ('star-rating Four'):
-        print('Rating is 1/5')
-    case ('star-rating Five'):
-        print('Rating is 1/5')
-    case _:
-        print('Not rating')  
+def findRating():
+
+    rating = soup.select_one('p', class_ = 'star-rating One') 
     
+    rating_class = rating.get('class')
+    
+# rating_class será una lista como ['star-rating', 'One']
+    print("Clases recuperadas:", rating_class)
+    
+    
+    match rating_class :
+         case ['star-rating', 'One']:
+             return 'Rating is 1/5'
+         case ['star-rating', 'Two']:
+             return 'Rating is 2/5'
+         case ['star-rating', 'Three']:
+             return 'Rating is 3/5'
+         case ['star-rating', 'Four']:
+             return 'Rating is 4/5'
+         case ['star-rating', 'Five']:
+             return 'Rating is 5/5'
+        
+         case _:
+               return'Not rating'
+    return rating_class 
 
   
+final_rating = findRating()
 
-        
         
 with open('infos.csv', 'w') as file:
    for row in infos_product:
@@ -79,14 +89,16 @@ with open('infos.csv', 'w') as file:
     if detail_title and detail_info:
         file.write("\n" + Title_Book.get_text(strip=True))
         file.write("\n" + description_book[0].get_text(strip=True))
-        file.write("\n" + rating.get_text(strip=True))
+        file.write("\n" + final_rating)  
         file.write("\n" + stock.get_text(strip=True))
         file.write("\n" + "Product Information : ")
         file.write('\n' + "Espec is : " + detail_title.get_text(strip=True) + " : " + detail_info.get_text(strip=True))
         print('\n' "Espec is : " + detail_title.get_text(strip=True) + " : " + detail_info.get_text(strip=True))
     else:
         file.write("Pas de detail")
-    
+
+
+print(f'\n\n\n\n' + str(final_rating))
 
 
 
