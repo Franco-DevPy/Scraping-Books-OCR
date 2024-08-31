9b42ae2964986c044e5d968457f2e2adcbc5c6e4
Franco-M.py <156203677+Franco-DevPy@users.noreply.github.com>
2024-08-31 16:27:54 +0200

    Update main.py
    Deleted comment

--- main.py
+++ main.py
@@ -13,19 +13,14 @@ def main(url):
     response = requests.get(url)
     html_response = response.text
     soup = BeautifulSoup(html_response, "html.parser")
-    # Get all book categories
     
     
     urls_all_category = find_category(soup)
-    #print(urls_all_category)
-    # Get all book categories
     all_book_for_category(urls_all_category)   
     
 
-    print('SCRAPING COMPLETED IN : ', url )
+    print('ETL COMPLETED')
 
 
-#permite que un archivo funcione tanto como un script 
-# ejecutable como un módulo importable sin conflictos.
 if __name__ == "__main__":
     main(url)
